107f66c17d5b041b279a4695267a9be069f2471a
nargizaladdinli <nargizalieva05@gmail.com>
2023-10-17 17:23:05 +0400

    push

---
+++ homeq/src/components/layout/header/Header.jsx
@@ -0,0 +1,12 @@
+import style from './_header.module.scss'
+
+export default function Header() {
+    return (
+      <header>
+        <div className="container">
+            lorem
+        </div>
+      </header>
+    )
+  }
+  
